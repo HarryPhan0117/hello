b971e9bec487acb61a6dff3c84859b668a1ef5bb
HarryPhan0117 <cphan5@unl.edu>
2024-08-27 13:49:49 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Harry Phan
+ * Date: 2024/08/22
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("I love the School of Computing\n");
+
+  return 0;
+}
