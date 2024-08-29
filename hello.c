bc9c76341b58b4045bb737a079606edcf5a711d0
Phan Chí Hiếu <133566416+pchihieu@users.noreply.github.com>
2024-08-29 17:49:45 +0000

    alternate harry testing the commit

--- hello.c
+++ hello.c
@@ -11,6 +11,8 @@
 int main(int argc, char **argv) {
 
   printf("I love the School of Computing\n");
+  printf("I am Harry but on the second account\n");
+  printf("I am majoring in Computer Science");
 
   return 0;
 }
